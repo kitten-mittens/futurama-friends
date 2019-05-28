89020d00a2c62045cdbad6fd7f243dba4977e8a7
DannySuarez <suarezd10@gmail.com>
2019-05-28 15:31:40 -0700

    page loads data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import FuturamaList from '../components/FuturamaList.js';
+import api from '../services/futurama-api.js';
 
 class App extends Component {
 
@@ -14,33 +16,25 @@ class App extends Component {
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
 
+        // this renders main list
+        const futuramaList = new FuturamaList({ friends: [] });
+        main.appendChild(futuramaList.render());
+
+        api.getFriends()
+            .then(friends => {
+                futuramaList.update({ friends });
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
         return dom;
     }
-
-
 
     renderTemplate() {
         return /*html*/`
             <div>
                 <main>
-                    <ul class="futurama-list">
-                        <li class="futurama-item">
-                            <h2>Dr Zoidberg</h2>
-                            <img src="https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png" alt="Dr Zoidberg">
-                            <blockquote>"I don't like the looks of this doctor. I bet I've lost more patients than he's\neven treated."</blockquote>
-                        </li>
-                        <li class="futurama-item">
-                            <h2>Dr Zoidberg</h2>
-                            <img src="https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/Dr_John_Zoidberg.png" alt="Dr Zoidberg">
-                            <blockquote>"...it's what seperates humans and robots from animals and animal robots."</blockquote>
-                        </li>
-                        <li class="futurama-item">
-                            <h2>Dr Zoidberg</h2>
-                            <img src="https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904047/calculon.jpg" alt="Dr Zoidberg">
-                            <blockquote>"...it's what seperates humans and robots from animals and animal robots."</blockquote>
-                        </li>
-                    </ul>
                 </main>
             </div>
         `;
--- src/components/FuturamaList.js
+++ src/components/FuturamaList.js
@@ -1,6 +1,17 @@
 import Component from './Component.js';
+import FuturamaItem from './FuturamaItem.js';
 
 class FuturamaList extends Component {
+    render() {
+        const list = this.renderDOM();
+
+        this.props.friends.forEach(friend => {
+            const futuramaItem = new FuturamaItem({ friend });
+            list.appendChild(futuramaItem.render());
+        });
+
+        return list;
+    }
 
     renderTemplate() {
         return /*html*/ `
